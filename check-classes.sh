2646f1be0d20553bbb88a184b740d1a5ca5f0261
Karl Heinz Marbaise <info@soebes.de>
2011-03-22 06:10:15 +0100

    - Only select files which really have .class   extension incl. the dot.

--- check-classes.sh
+++ check-classes.sh
@@ -3,7 +3,7 @@
 # An other solution might be to use file instead.
 #  find -type f -name "*.class" | xargs file | grep -v "compiled Java class data, version 50.0 (Java 1.6)"
 #
-for i in `find . -name \*class`
+for i in `find . -name \*\.class`
 do
   v=`od -h $i | head -1 | awk '{print $5}'`
   if [ $v != "3100" ]
