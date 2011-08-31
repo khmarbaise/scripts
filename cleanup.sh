d99d462a703fab46c68d3c5f4c7194bf38edf9d0
Karl Heinz Marbaise <info@soebes.de>
2011-08-31 14:28:59 +0200

    - Added -f force option to prevent error message   in case of not existing files/folders - Added .metadata folder.

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Remove all files which are related to Eclipse configuration
 #
-find -type f -name ".classpath" | xargs rm
-find -type f -name ".project" | xargs rm
+find -type f -name ".classpath" | xargs rm -f
+find -type f -name ".project" | xargs rm -f
 find -type d -name ".settings" | xargs rm -fr
+find -type d -name ".metadata" | xargs rm -fr
