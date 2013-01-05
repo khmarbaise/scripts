2956932b1784b8ed50545db05898a9bd73a8f273
Karl Heinz Marbaise <info@soebes.de>
2013-01-05 01:13:48 +0100

    - Now sorting of ip addresses is working correctly.

--- check-iptables.sh
+++ check-iptables.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 DATE=`date  +%Y%m%d%H%M%S`
 NUMBER_OF_DENIES=`cat /var/log/syslog | grep "iptables denied:" | wc -l`
-LIST_OF_IPS=`cat /var/log/syslog | grep "iptables denied:" | tr -s " " | cut -d" " -f12 | cut -d "=" -f2 | sort | uniq | tr "\\n" "," | sed 's/,$//'`
+##LIST_OF_IPS=`cat /var/log/syslog | grep "iptables denied:" | tr -s " " | cut -d" " -f12 | cut -d "=" -f2 | sort | uniq | tr "\\n" "," | sed 's/,$//'`
+#LIST_OF_IPS=`cat /var/log/syslog | grep "iptables denied:" | tr -s " " | cut -d" " -f12 | cut -d "=" -f2 | sort | uniq | paste -sd ","`
+LIST_OF_IPS=`cat /var/log/syslog | grep "iptables denied:" | tr -s " " | cut -d" " -f12 | cut -d "=" -f2 | sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n | uniq | paste -sd ","`
 
 echo "${DATE}|${NUMBER_OF_DENIES}|${LIST_OF_IPS}"
 
