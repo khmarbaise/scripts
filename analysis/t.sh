85933c8532ddf101a6e632e8449b9624702ec056
Karl Heinz Marbaise <khmarbaise@apache.org>
2016-01-02 14:01:06 +0100

    Simplified

--- analysis/t.sh
+++ analysis/t.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-WORDS=$(cat text.txt | sort -u | cut -d" " -f1 | uniq)
+WORDS=$(cat text.txt | cut -d" " -f1 | sort -u)
 for i in $WORDS; do
   echo -n "$i:"
   VALUE=`cat text.txt | egrep "^${i}\s+" | cut -d' ' -f2 | paste -sd+ - | bc -l`
